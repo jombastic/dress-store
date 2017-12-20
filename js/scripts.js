cacee181057e85d46c0201e30ae04b93301f8e15
Slavcho Mitrov <slavemitrov@yahoo.com>
2017-12-20 21:40:13 +0100

    Add logic for #product form

--- js/scripts.js
+++ js/scripts.js
@@ -4,4 +4,24 @@ $(function() {
     $("img").hide();
     $("img#" + clickedRadio).show();
   });
+
+  $("#product").submit(function(event) {
+    event.preventDefault();
+
+    var dressColor = $("input:radio[name=dressColor]:checked").val();
+    var dressSize = $("#dressSize").val();
+
+    if (dressColor === "red") {
+      $("#picture").html("<img src='img/red-dress.jpg' class='img-responsive' />");
+    } else if (dressColor === "blue") {
+      $("#picture").html("<img src='img/blue-dress.jpg' class='img-responsive' />");
+    } else if (dressColor === "yellow") {
+      $("#picture").html("<img src='img/yellow-dress.jpg' class='img-responsive' />");
+    }
+
+    $("#chosenColor").text(dressColor);
+    $("#chosenSize").text(dressSize);
+
+    $("fieldset").removeAttr("disabled");
+  });
 });
